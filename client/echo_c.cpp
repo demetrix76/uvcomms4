da2b2ab5e561d98c4138325b5a9c8d1aafee5297
Dmitry Ovcharenko <dmitry.ovcharenko@gmail.com>
2022-12-11 23:37:13 +0300

    Cleanup

--- client/echo_c.cpp
+++ client/echo_c.cpp
@@ -1,5 +1,4 @@
 
-#include <commlib/client.h>
 #include <commlib/piper.h>
 #include <commlib/commlib.h>
 
@@ -16,119 +15,12 @@
 
 using namespace uvcomms4;
 
-class EchoClientDelegate: public ClientDelegate
+class EchoClientDelegate: public PiperDelegate
 {
 public:
     static constexpr int repeat_count = 100;
 
     EchoClientDelegate(std::promise<void> && aCompletionPromise) :
-        mCompletionPromise(std::move(aCompletionPromise))
-    {}
-
-    void onStartup(Client *aClient) override
-    {
-        // reminder: Constructor thread
-        mClient = aClient;
-
-        aClient->connect(pipe_name(config::get_default()),[this](auto result){
-            using namespace std::literals;
-            auto [descriptor, status] = result;
-            if(0 == status)
-            {
-                sendRandomMessage(descriptor, repeat_count);
-            }
-            else
-            {
-                throw std::runtime_error("Failed to connect");
-            }
-        });
-    }
-
-    void onShutdown() override
-    {
-        // reminder: Destructor thread
-    }
-
-    void onMessage(Descriptor aDescriptor, Collector & aCollector) override
-    {
-        // reminder: IO thread
-        // we MUST extract the message here; otherwise, we'll have an infinite loop
-        auto [status, message] = aCollector.getMessage<std::string>();
-        if(status == CollectorStatus::HasMessage)
-        {
-            if(!checkExpectedMessage(message))
-                throw std::runtime_error("Messages do not match");
-        }
-    }
-
-    void onNewPipe(Descriptor aDescriptor) override
-    {
-        // reminder: IO thread
-    }
-
-    void onPipeClosed(Descriptor aDescriptor, int aErrorCode) override
-    {
-        // reminder: IO thread
-        if(aErrorCode)
-            std::cerr << "Pipe error: " << aErrorCode << std::endl;
-    }
-
-    // =========================
-
-    void addExpectedMessage(std::string const &aMessage)
-    {
-        std::lock_guard lk(mMx);
-        mExpectedMessages.push_back(aMessage);
-    }
-
-    bool checkExpectedMessage(std::string const &aMessage)
-    {
-        std::string front_msg;
-        std::lock_guard lk(mMx);
-        {
-            if(mExpectedMessages.empty())
-                return false;
-            front_msg = std::move(mExpectedMessages.front());
-            mExpectedMessages.pop_front();
-        }
-        if(0 == --mCounter)
-            mCompletionPromise.set_value();
-        return front_msg == aMessage;
-    }
-
-    void sendRandomMessage(Descriptor aDescriptor, int aRemaining)
-    {
-        std::cout << aRemaining << std::endl;
-        if(aRemaining > 0)
-        {
-            std::string s = "Some Message";
-            addExpectedMessage(s);
-            mClient->send(aDescriptor, std::move(s), [=, this](int aErrCode){
-                if(aErrCode)
-                    throw std::system_error(std::error_code(-aErrCode, std::system_category()), "SEND");
-                else
-                    sendRandomMessage(aDescriptor, aRemaining - 1);
-            });
-        }
-    }
-
-private:
-    Client       *mClient { nullptr };
-    int           mCounter { repeat_count };
-
-    std::mutex              mMx;
-    std::deque<std::string> mExpectedMessages;
-
-    std::promise<void>      mCompletionPromise;
-};
-
-
-class EchoClientDelegate2: public PiperDelegate
-{
-public:
-    static constexpr int repeat_count = 100;
-
-    EchoClientDelegate2(std::promise<void> && aCompletionPromise) :
         mCompletionPromise(std::move(aCompletionPromise))
     {}
 
@@ -225,13 +117,6 @@ private:
 
 void run_echo_client()
 {
-    // std::promise<void> completionPromise;
-    // auto completionFuture = completionPromise.get_future();
-    // Client client(config::get_default(), std::make_shared<EchoClientDelegate>(std::move(completionPromise)));
-
-    // completionFuture.get();
-    //config const & cfg = config::get_default();
-
     constexpr std::size_t client_count = 100;
 
     std::list<std::promise<void>> completionPromises;
@@ -246,7 +131,7 @@ void run_echo_client()
     std::list<std::unique_ptr<Piper>> clients;
     std::transform(completionPromises.begin(), completionPromises.end(), std::back_insert_iterator(clients),
         [&](std::promise<void> & pms){
-            return std::make_unique<Piper>(std::make_shared<EchoClientDelegate2>(std::move(pms)));
+            return std::make_unique<Piper>(std::make_shared<EchoClientDelegate>(std::move(pms)));
         });
 
     for(auto & f: completionFutures)
--- client/main.cpp
+++ client/main.cpp
@@ -1,54 +1,7 @@
 #include <iostream>
 
-#include <commlib/client.h>
 #include <commlib/piper.h>
 #include <string>
-
-class SampleClientDelegate: public uvcomms4::ClientDelegate
-{
-public:
-
-    void onStartup(uvcomms4::Client *aClient) override
-    {
-        // reminder: Constructor thread
-        mClient = aClient;
-        std::cout << "[SampleClient] Startup\n";
-
-        // normally, the IO loop will only start after this method finishes,
-        // so we shouldn't make any blocking calls that depend on the IO loop;
-        // or, call unlockIO() first once we're done initializing
-    }
-
-    void onShutdown() override
-    {
-        // reminder: Destructor thread
-        std::cout << "[SampleClient] Shutdown\n";
-    }
-
-    void onMessage(uvcomms4::Descriptor aDescriptor, uvcomms4::Collector & aCollector) override
-    {
-        // reminder: IO thread
-        // we MUST extract the message here; otherwise, we'll have an infinite loop
-        auto [status, message] = aCollector.getMessage<std::string>();
-        if(status == uvcomms4::CollectorStatus::HasMessage)
-            std::cout << "[SampleClient] MESSAGE: " << message << std::endl;
-    }
-
-    void onNewPipe(uvcomms4::Descriptor aDescriptor) override
-    {
-        // reminder: IO thread
-        std::cout << "[SampleClient] New pipe: " << aDescriptor << std::endl;
-    }
-
-    void onPipeClosed(uvcomms4::Descriptor aDescriptor, int aErrorCode) override
-    {
-        // reminder: IO thread
-        std::cout << "[SampleClient] Pipe closed: " << aDescriptor << "; error code " << aErrorCode << std::endl;
-    }
-
-private:
-    uvcomms4::Client       *mClient { nullptr };
-};
 
 void run_echo_client();
 
@@ -116,22 +69,7 @@ int main(int, char*[])
         std::cout << "Write result " << wrstatus << std::endl;
     }
 
-    // uvcomms4::Client client(cfg, std::make_shared<SampleClientDelegate>());
-
-    // client.connect(uvcomms4::pipe_name(cfg), [&](auto result)mutable {
-    //     using namespace std::literals;
-    //     auto [descriptor, status] = result;
-    //     if(0 == status)
-    //         client.send(descriptor, "Wilkommen Bienvenue Welcome"s, [](int){});
-    // });
-
-    // auto [descriptor, status] = client.connect(uvcomms4::pipe_name(cfg)).get();
-
-    // if(0 == status)
-    //     client.send(descriptor, "Wilkommen Bienvenue Welcome"s, [](auto){});
-
     std::this_thread::sleep_for(std::chrono::seconds(1));
-
 
     return 0;
 }
